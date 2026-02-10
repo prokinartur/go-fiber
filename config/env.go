7e6f35c95f3bed62e4de1fb295c7914318c0aa66
Artur Prokin <prokin.artur@gmail.com>
2026-02-10 17:34:40 +0700

    Начало работы с логированием + уровни логирования для разных окружений

--- config/env.go
+++ config/env.go
@@ -46,13 +46,23 @@ func getBool(key string, defaultValue bool) bool {
 }
 
 type DatabaseConfig struct {
-	url string
+	Url string
+}
+
+type LogConfig struct {
+	Level int
+}
+
+func NewLogConfig() *LogConfig {
+	return &LogConfig{
+		Level: getInt("LOG_LEVEL", 0),
+	}
 }
 
 // Default value
 // Функции для получения int и bool
 func NewDatabaseConfig() *DatabaseConfig {
 	return &DatabaseConfig{
-		url: getString("DATABASE_URL", ""),
+		Url: getString("DATABASE_URL", ""),
 	}
 }
